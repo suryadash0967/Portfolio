eb8126b9a3970c8197201262b15332214e9f33fb
suryadash0967 <surya.dash0967@gmail.com>
2026-09-19 02:21:16 +0530

    years old modified

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -111,11 +111,6 @@ export default function About() {
 
             <div className={styles.statsArea} ref={statsRef}>
               <div className={styles.statGroup}>
-                <h3 className={styles.statNumber}>20 YO</h3>
-                <p className={styles.statLabel}>TOO YOUNG? TWSS</p>
-              </div>
-              
-              <div className={styles.statGroup}>
                 <h3 className={styles.statNumber} ref={numberRef}>0+</h3>
                 <p className={styles.statLabel}>USERS IMPACTED</p>
               </div>
@@ -125,6 +120,11 @@ export default function About() {
                   <span>NOT</span> <span>BAD</span>
                 </h3>
                 <p className={styles.statLabel}>CGPA</p>
+              </div>
+
+              <div className={styles.statGroup}>
+                <h3 className={styles.statNumber}>20 YEARS OLD</h3>
+                <p className={styles.statLabel}>TOO YOUNG? TWSS.</p>
               </div>
               
               <div className={styles.statGroup}>
